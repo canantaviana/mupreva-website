2abd2c8c77d1d8242be0eccba4dd3113b72bdeb0
Carles Briansó <carles.brianso@antaviana.cat>
2025-03-18 10:38:11 +0100

    canviado sistema para quitar acentos en los templates, para evitar problemas con iconv

--- web_app/common/class.page.php
+++ web_app/common/class.page.php
@@ -238,7 +238,6 @@ class page
 
 
     private function defaultTemplateData($value) {
-        //$template_name = str_replace(' ', '_', strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $value->name)));
         $template_name = $value->name;
         return '{
             "id": "'.$template_name.'",
@@ -1189,8 +1188,9 @@ class page
             }
             $template_name = $template_map->template;
         }
-        $template_name = str_replace(' ', '_', strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $template_name)));
-
+        $template_name = str_replace(' ', '_', strtolower($this->remove_accents($template_name)));
+var_dump($template_name);
+exit();
         #
         # TEMPLATE CSS / JS
         if ($options->add_template_css === true) {
@@ -1475,6 +1475,23 @@ class page
         return $items;
     } //end get_children
 
+    protected function remove_accents($str) {
+        $accents = [
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A',
+            'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a',
+            'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E',
+            'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e',
+            'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I',
+            'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i',
+            'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O',
+            'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o',
+            'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U',
+            'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u',
+            'Ñ' => 'N', 'ñ' => 'n',
+            'Ç' => 'C', 'ç' => 'c'
+        ];
 
+        return strtr($str, $accents);
+    }
 
 }//end class page
