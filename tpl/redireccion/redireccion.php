e9af9f1a1bd01abd03479a1ac0ab619603dc68eb
Carles Briansó <carles.brianso@antaviana.cat>
2025-05-27 11:55:48 +0200

    arreglat bug a redireccions

--- tpl/redireccion/redireccion.php
+++ tpl/redireccion/redireccion.php
@@ -11,25 +11,13 @@
 // page basic vars
 $abstract = $this->get_element_from_template_map('abstract', $template_map->{$mode});
 if ($abstract) {
-    if (strpos($abstract, 'http') === 0) {
-        header("Location: ".$abstract);
-        exit();
-    } else {
-        header("Location: /".$abstract);
-        exit();
-    }
+    header("Location: ".$abstract);
 }
 $menu_tree = $this->get_menu_tree_plain(WEB_MENU_PARENT, []);
 $childrens = $this->get_children($this->row->term_id, $menu_tree);
 if ($childrens && count($childrens) > 0) {
     $next = $childrens[0];
     $url = $next->web_path;
-    if (strpos($url, 'http') === 0) {
-        header("Location: ".$url);
-        exit();
-    } else {
-        header("Location: /".$url);
-        exit();
-    }
+    header("Location: /".$url);
 }
 exit();
