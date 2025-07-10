20165bd887eb320a244935dce4ebcd4644381c7b
Albert Bonmassip <albert.bonmassip@antaviana.cat>
2025-07-10 08:35:22 +0200

    Arreglar peu de foto de tots els popups

--- tpl/page/js/modules.js
+++ tpl/page/js/modules.js
@@ -52,7 +52,7 @@ var templateModules = {
                                     </svg>
                                 </button>
                                 <img loading="lazy" src="${info.image[0].image.replace('1.5MB', 'original')}" alt="" class="is-block">
-                                <p>${info.image[0].footprint}</p>
+                                <p class="has-text-centered mt-2">${info.image[0].footprint}</p>
                             </div>
                         </div>
                     </div>`
@@ -133,7 +133,7 @@ var templateModules = {
                                             </svg>
                                         </button>
                                         <img loading="lazy" src="${info.image[0].image.replace('1.5MB', 'original')}" alt="" class="is-block">
-                                        <p>${info.image[0].footprint}</p>
+                                        <p class="has-text-centered mt-2">${info.image[0].footprint}</p>
                                     </div>
                                 </div>
                             </div>`
@@ -179,7 +179,7 @@ var templateModules = {
                                             </svg>
                                         </button>
                                         <img loading="lazy" src="${info.image[0].image.replace('1.5MB', 'original')}" alt="" class="is-block">
-                                        <p>${info.image[0].footprint}</p>
+                                        <p class="has-text-centered mt-2">${info.image[0].footprint}</p>
                                     </div>
                                 </div>
                             </div>`
@@ -350,7 +350,7 @@ var templateModules = {
                                     </svg>
                                 </button>
                                 <img loading="lazy" src="${info.image[0].image.replace('1.5MB', 'original')}" alt="" class="is-block">
-                                <p>${info.image[0].footprint}</p>
+                                <p class="has-text-centered mt-2">${info.image[0].footprint}</p>
                             </div>
                         </div>
                     </div>`
